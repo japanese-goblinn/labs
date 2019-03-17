3a7d001464c87357cca3def126dcf6858a78e2a5
saisuicied <cool45akol@gmail.com>
2019-03-17 21:25:00 +0300

    added book django model

---
+++ term4/OOP/main/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from library_base_classes import Book
+
+
+class BookPost(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    year = models.IntegerField()
+    amount_of_pages = models.IntegerField()
+    genres = models.CharField(max_length=200)
