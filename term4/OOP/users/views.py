b01fbcca254280d840fca286c0af8b2d8a3e7d10
saisuicied <cool45akol@gmail.com>
2019-03-22 00:18:14 +0300

    finished login/logout system

--- term4/OOP/users/views.py
+++ term4/OOP/users/views.py
@@ -9,9 +9,9 @@ def register(request):
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
+            messages.success(request, f'Account created for {username}! You\'re now able to log in!')
             form.save()
-            return redirect('home')
+            return redirect('login')
     else:
         form = UserRegistrationForm()
     return render(request, 'users/register.html', {'form': form})
