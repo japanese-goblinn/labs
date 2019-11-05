41b9cbf0f021bf8c117ad8fd47709ddb4ec1eb9a
Kirill Gorbachyonok <cool45akol@gmail.com>
2019-11-06 00:49:08 +0300

    init

---
+++ term5/AVS/avs3/cmake-build-debug/CMakeFiles/avs3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/avs3.dir/main.cpp.o"
+  "avs3"
+  "avs3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/avs3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
