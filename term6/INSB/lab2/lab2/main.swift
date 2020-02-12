0dc0984e860edaf7efbd7fc4c0db54126c30de5a
japanese-goblinn <cool45akol@gmail.com>
2020-02-12 20:27:18 +0300

    Update ⬆️

---
+++ term6/INSB/lab2/lab2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  lab2
+//
+//  Created by Kirill Gorbachyonok on 2/12/20.
+//  Copyright © 2020 Kirill Gorbachyonok. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
