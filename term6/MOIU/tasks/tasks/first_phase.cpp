eff19f5b515c06b937ba33a31cb692eaf6eced37
japanese-goblinn <cool45akol@gmail.com>
2020-04-02 23:15:17 +0300

    FEAT: start c++ implementation

---
+++ term6/MOIU/tasks/tasks/first_phase.cpp
@@ -0,0 +1,9 @@
+//
+//  first_phase.cpp
+//  tasks
+//
+//  Created by Kirill Gorbachyonok on 4/1/20.
+//  Copyright © 2020 Kirill Gorbachyonok. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ term6/MOIU/tasks/tasks/main_phase.cpp
@@ -0,0 +1,17 @@
+//
+//  simplex_main.cpp
+//  simplex_main
+//
+//  Created by Kirill Gorbachyonok on 4/1/20.
+//  Copyright © 2020 Kirill Gorbachyonok. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main(int argc, const char * argv[]) {
+    cout << "HI" << endl;
+    return 0;
+}
