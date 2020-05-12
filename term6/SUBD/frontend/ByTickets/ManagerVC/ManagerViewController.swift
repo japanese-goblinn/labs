a50135dd594ecba029d88ef9f9e80c2d4a0327c8
japanese-goblinn <cool45akol@gmail.com>
2020-05-12 03:57:28 +0300

    feat: manager screen finished

--- term6/SUBD/frontend/ByTickets/ManagerVC/ManagerViewController.swift
+++ term6/SUBD/frontend/ByTickets/ManagerVC/ManagerViewController.swift
@@ -8,15 +8,29 @@
 
 import Cocoa
 
+struct Count: Codable {
+    let amount: Int
+}
+
 class ManagerViewController: NSViewController {
     
     var user: User = .defaultUser(with: 0)
     
+    @IBOutlet weak var ticketsAmount: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        Database.callProcedure("amount_of_tickets", with: [user.username]) {
+            [weak self] (res: Result<[Count], RequestError>) -> Void in
+            guard let self = self else { return }
+            switch res {
+            case .success(let amount):
+                self.ticketsAmount.stringValue = "\(amount.first?.amount ?? 0)"
+            case .failure(let error):
+                self.showAlert(title: "TICKETS LOAD ERROR", content: error.error, buttonText: "OK", style: .critical)
+            }
+        }
     }
-    
 }
 
 extension ManagerViewController: Userable {}
