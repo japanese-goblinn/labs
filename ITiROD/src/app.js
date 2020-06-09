9cd11916f708c643696a2f7d738094904105c5a6
japanese-goblinn <cool45akol@gmail.com>
2020-06-09 21:47:40 +0300

    feat: add firebase

--- ITiROD/src/app.js
+++ ITiROD/src/app.js
@@ -1,6 +1,9 @@
-import Router from './router.js';
-import Renderer from './renderer.js';
+import { firebaseConfig } from './scripts/firebaseConfig.js';
 
+import Router from './scripts/router.js';
+import Renderer from './scripts/renderer.js';
+
+firebase.initializeApp(firebaseConfig);
 export const mobile = window.matchMedia('screen and (max-width: 800px)');
 export const renderer = new Renderer();
 export const router = new Router();
